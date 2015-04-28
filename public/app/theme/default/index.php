55db7ec6b7120d5a96ff8aed377f027e32f592b6
nagayama <nagayama@iroirodesign.com>
2015-04-28 10:58:55 +0900

    add header and footer

--- public/app/theme/default/index.php
+++ public/app/theme/default/index.php
@@ -2,10 +2,22 @@
 
 $context = Timber::get_context();
 $context['post'] = new TimberPost();
+$context['posts'] = Timber::get_posts();
+$context['pagination'] = Timber::get_pagination();
+$context['description'] = ""; 
+
 $templates = ['index'];
 
-if ( is_home() ) {
+if ( is_404() ) {
+  array_unshift( $templates, '404' );
+} elseif ( is_home() ) {
   array_unshift( $templates, 'home' );
+} elseif ( is_singular() ) {
+  array_unshift( $templates, 'single' );
+} elseif ( is_archive() ) {
+  array_unshift( $templates, 'archive' );
+} elseif ( is_page() ) {
+  array_unshift( $templates, 'page' );
 }
 
 $templates = array_map(function($template) {
